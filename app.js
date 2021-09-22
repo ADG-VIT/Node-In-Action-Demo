6b2621e2b73dcca9032b9a38f93937f2abcee7ea
Arjun <arjun140702@gmail.com>
2021-09-22 18:31:20 +0530

    Add code to some routes

--- app.js
+++ app.js
@@ -9,7 +9,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
-app.get("/", (res) => {
+let collection;
+const client = new MongoClient(data.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+try {
+    client.connect(async err => {
+        if (err) {
+            console.log(err);
+        } else {
+            collection = await client.db().collection();
+        }
+    });
+} catch (e) {
+    console.log(e);
+}
+
+app.get("/", async (res) => {
+    if (collection !== undefined)
+        let notesList = await collection.find({}).toArray();
     res.render('home', {notes : notesList});
 });
 
@@ -18,6 +35,12 @@ app.get("/new", (res) => {
 });
 
 app.post("/new", (req, res) => {
+    const data = req.body;
+    const note = {title: data.title, body: data.body};
+
+    if (collection !== undefined)
+        response = await collection.insertOne(note);
+          
     res.redirect("/");
 });
 
