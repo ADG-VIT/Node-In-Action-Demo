590436d8666929b6596e23fd7d9d86eee3921771
Arjun <arjun140702@gmail.com>
2021-09-22 18:24:09 +0530

    Basic imports and routes setup

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const ejs = require('ejs');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get("/", (res) => {
+    res.render('home', {notes : notesList});
 });
 
-app.listen(4000, () => {
-    console.log("Server started");
+app.get("/new", (res) => {
+    res.sendFile(__dirname + "/new-note.html");
+});
+
+app.post("/new", (req, res) => {
+    res.redirect("/");
+});
+
+app.get("/note", (res) => {
+    res.render('notes');
+});
+
+app.post("/note", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
+    console.log("Server started at port 3000");
 });
